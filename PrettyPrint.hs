5f97ba446bcaff6ce0aed715cee74cbf5442a0b7
Cirioke <joeldukowski@gmail.com>
2021-03-01 16:57:23 +0100

    Shortened the lines in to < 80 PrettyPrint.hs

--- PrettyPrint.hs
+++ PrettyPrint.hs
@@ -3,7 +3,8 @@ module PrettyPrint where
 import Type
 
 -- Type class to define the function "pretty".
--- The function "pretty" gives the Prolog representation of an object in Haskell as a string.  
+-- The method "pretty" returns the Prolog representation
+-- of an object in Haskell as a string.  
 class Pretty a where
   pretty :: a -> String
 
@@ -11,11 +12,14 @@ class Pretty a where
 instance Pretty Term where
   pretty (Var  (VarName a)   ) = a                  
   pretty (Comb "." (a:(b:[]))) = "[" ++ pretty a ++ nexts b ++ "]"
-    where
-      nexts :: Term -> String
-      nexts (Comb "."  (a:(b:[]))) = ", " ++ pretty a ++ nexts b
-      nexts (Comb "[]" _         ) = ""
-      nexts a                      = "|" ++ pretty a
-  pretty (Comb a []) = a  
-  pretty (Comb a (b:bs)) = a ++ "(" ++ (foldl (\x b -> x ++ ", " ++ pretty b) (pretty b) bs) ++ ")"
+   where
+    nexts :: Term -> String
+    nexts (Comb "."  (a:(b:[]))) = ", " ++ pretty a ++ nexts b
+    nexts (Comb "[]" _         ) = ""
+    nexts a                      = "|" ++ pretty a
+
+  pretty (Comb a   []        ) = a  
+  pretty (Comb a   (b:bs)    ) = a ++ "(" ++ joined b bs ++ ")"
+   where
+    joined b bs = (foldl (\x b -> x ++ ", " ++ pretty b) (pretty b) bs)
 
