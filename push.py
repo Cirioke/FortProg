9a11e93fa4fcf9556e5d387141fc82f0649513d4
Cirioke <joeldukowski@gmail.com>
2021-03-03 10:39:30 +0100

    ...

--- push.py
+++ push.py
@@ -4,5 +4,6 @@ import os
 os.system("git pull")
 os.system("git add .")
 os.system('git commit -m "'+sys.argv[1]+'"')
-os.system("git push\nCirioke\nf42tkb91gb")
+os.system("git push")
+#os.system("git push\nCirioke\nf42tkb91gb")
 
