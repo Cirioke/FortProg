9bdc5e835418a426ff6901f568d24308f9e4ad77
Cirioke <79838210+Cirioke@users.noreply.github.com>
2021-03-03 10:44:42 +0100

    Delete push.py

--- push.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-os.system("git pull")
-os.system("git add .")
-os.system('git commit -m "'+sys.argv[1]+'"')
-os.system("git push")
-#os.system("git push\nCirioke\nf42tkb91gb")
-
