91f1ac49fb7b780f943054a00bcc398bcb042f3e
Cirioke <joeldukowski@gmail.com>
2021-03-03 10:52:54 +0100

    ...

--- make_hs_path.ps1
+++
@@ -1,7 +0,0 @@
-$path=""
-$base_path_win = Get-Location
-$base_path_nix = ($base_path_win -replace "\\","/").Trim("/")
-foreach($line in Get-Content .\HASKELL_PATH.txt) {
-  $path = "$path"+":$base_path_nix$line"
-}
-$path
