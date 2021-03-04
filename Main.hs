e12303180c168f6420746d6dc271ce44829c0419
stu223753 <stu223753@mail.uni-kiel.de>
2021-03-04 18:27:09 +0100

    Warings Main

--- Main.hs
+++ Main.hs
@@ -2,18 +2,33 @@ module Main where
 
 import Type
 import PrettyPrint
--- import Renaming
-import SetsAsOrderedList
+import Renaming
+import qualified SetsAsOrderedList
 -- import SLDResolution
 import Substitutions
--- import Unification
---  import AnonymVars
+import Unification
+import AnonymVars
 import Variables
 
 
-a :: Term
-a = Comb "f" [Var (VarName "A"), Var (VarName "B")]
+set :: SetsAsOrderedList.Set Int
+set = SetsAsOrderedList.empty
+
+subst :: Subst
+subst = empty
+
+anonym :: Bool
+anonym = isAnonym (VarName "XZY")
+
+term :: Term
+term = Comb "f" [Var (VarName "A"), Var (VarName "B")]
+
+renamed_term::Term
+renamed_term = rename [] term
+
+unificator :: Maybe Subst
+unificator = unify term renamed_term
 
 main :: IO ()
-main = do putStrLn (pretty a)
-          print (allVars a)
+main = do putStrLn (pretty renamed_term)
+          print (allVars renamed_term)
