237c27b078a6e5cc622b1259bf247db678a8e624
stu223753 <stu223753@mail.uni-kiel.de>
2021-03-09 14:06:26 +0100

    sld_childs

--- Modules/SLDResolution/scr/SLDResolution.hs
+++ Modules/SLDResolution/scr/SLDResolution.hs
@@ -6,6 +6,15 @@ import Type
 
 data SLDTree = SLDTree Goal [(Subst,SLDTree)]
 
+sld_childs :: Prog -> Goal -> [(Subst,SLDTree)]
+sld_childs prog@(Prog rules) (Goal terms) = 
+  do literal   <- terms
+     Rule c as <- rules
+     mcu       <- unify c literal
+     new_goal  <- replace terms literal (apply mcu as)
+     return (mcu, sld prog new_goal)
+
+
 genSubs :: [Rule] -> [Term] -> [(Subst, SLDTree)]
 
 genSubs ((Rule rt rts):rs) (Goal (t:ts)) = if isNothing (unify (rename rt) t) 
