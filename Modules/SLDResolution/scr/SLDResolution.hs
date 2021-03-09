44bb5d725905ccd3a4949578150866ca37bb123a
Cirioke <joeldukowski@gmail.com>
2021-03-09 15:01:21 +0100

    concept for sld

--- Modules/SLDResolution/scr/SLDResolution.hs
+++ Modules/SLDResolution/scr/SLDResolution.hs
@@ -1,21 +1,32 @@
 module SLDResolution where
 
+import Data.Maybe
 import Substitutions
 import SetsAsOrderedList
 import Type
+import Unification
+import Renaming
+import Variables
 
+-- 1.
 data SLDTree = SLDTree Goal [(Subst,SLDTree)]
 
 
+-- 2.
 sld :: Prog -> Goal -> SLDTree
-sld p g = SLDTree g (sld_childs p g)
+sld p g = SLDTree g (childs (rename (allVars g) p) g)
  where 
-  sld_childs :: Prog -> Goal -> [(Subst,SLDTree)]
-  sld_childs prog@(Prog rules) (Goal terms) = 
-    do literal   <- terms
-       Rule c as <- rules
-       mcu       <- unify c literal
-       new_goal  <- replace terms literal (apply mcu as)
-       return (mcu, sld prog new_goal)
+  childs :: Prog -> Goal -> [(Subst,SLDTree)]
+  childs prog@(Prog rules) (Goal (literal:terms)) = 
+    do Rule c as <- rules
+       mcu       <- maybeToList(unify c literal)
+       return (mcu, sld prog (Goal (apply mcu (as ++ terms))))  
+  childs _ (Goal []) = []
 
+
+-- 3.
+type Strategy = SLDTree -> [Subst]
+
+-- dfs :: Strategy
+-- dfs tr 
 
