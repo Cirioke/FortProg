2b1bd50b6581e86dbe7318fdb4d5a010063d61a0
stu223753 <stu223753@mail.uni-kiel.de>
2021-03-10 17:15:28 +0100

    main

--- Main.hs
+++ Main.hs
@@ -1,43 +1,121 @@
 import Type
 import Parser
 
-import PrettyPrint
-import Variables
-import AnonymVars
-import Substitution
-import Unification
-import Renaming
+import Session
+import Substitutions
 import SLDResolution
 
 
 
+-- -----------------------------------------------------------------------------
+-- ********************** MAIN SECTION *****************************************
+-- _____________________________________________________________________________
 
-parse 
 main :: IO ()
-main = do 
-  line <- getLine
-  case line of 
-    (':'('h':arg) -> do putStrLn helpString
-    (':'('l':arg) -> do prog <- parseFile (trimWitheSpace arg) :: IO (Either String Prog)
-                        case prog of
-                          Left  e <- putStrLn edges
-                          Right p <- expectoCommando (trimWitheSpace arg, p)
-    (':'('q':arg) -> do putStrLn "Bye…"
-    (':'('r':arg) -> do prog <- parseFile currProgPath :: IO (Either String Prog)
-                        case prog of
-                          Left  e <- putStrLn edges
-                          Right p <- expectoCommando (currProgPath, p)
-    (':'('s':arg) -> do
-    _             -> case (parse line :: Either String Goal) of
-                       
-                       
-data Session = Session 
-                 Prog
-                 String
-                 Strategy
+main = do putHello
+          repl defaultSession
 
-instance Monad Session
+-- / Read-Evaluate-Print-Loop function for the commandline Interpreter.
+repl :: Session -> IO ()
+repl session | (not.isAlive) session = putStrLn "Bye bye."
+             | otherwise             =
+  do putStr "?- "
+    -- read
+    inp <- getLine 
+    case strip inp of 
+      -- inp is a command
+      (':':(c:arg)) -> do --evaluate and print
+                          newSession <- command c session (strip arg) 
+                          -- loop
+                          repl newSession 
+      -- inp is a query
+      goalStr       -> do evalQuery goalStr session
+                          repl session
+  
 
+-- -----------------------------------------------------------------------------
+-- ********************** COMMAND SECTION **************************************
+-- _____________________________________________________________________________
+
+
+-- \ This funktion handles different commands.
+-- each command has a character as identifyer.
+-- A command might be (or might not be) interessted 
+-- in an argument given as a string.
+-- The context of the Programm is given in the third Parameter
+command :: Char -> String -> Session -> IO Session
+
+-------------------------- QUIT ------------------------------------------------
+command 'q' _ session    = pure (kill session)
+
+-------------------------- HELP ------------------------------------------------
+command 'h' session _    = 
+  do putStrLn helpString
+     return session
+
+-------------------------- SET STRATEGEY ---------------------------------------
+command 's' session sstr = do handle getStrategy sstr
+  case sstr of
+    "dfs"   -> do return (setStrategy session dfs)
+    "bfs"   -> do return (setStrategy session bfs)
+    "iddfs" -> do return (setStrategy session iddfs)
+    -- Error Handling
+    other   -> putError (other ++ " is not a registered strategy.") session
+
+-------------------------- LOAD PROGRAMM ---------------------------------------
+command 'l' session path =
+  do _prog <- parseFile path :: IO (Either String Prog)
+     case _prog of
+       Right prog     -> 
+         do s  <- return (setPath session path)
+            s' <- return (setProg s       prog)
+            return s'
+        -- error Handling
+       Left  errorStr -> 
+         putError ("Programm could not be parsed: "++ errorStr) session
+
+-------------------------- RELOAD PROGRAMM -------------------------------------
+command 'r' session _    = getCommand 'l' session (getPath session)
+
+-------------------------- NOT DEFINED COMMAND ---------------------------------
+-- Error Handling
+command c   session _    = 
+  putError (':':(c:" is not a defined command.")) session
+
+
+-------------------------- EVALUATING QUERY ------------------------------------
+-- / Try to evaluate the given string as a query within the given session
+evalQuery :: String -> Session -> IO ()
+evalQuery goalStr session =  do
+  either handleError continue 
+  nextSol
+  case _goal of
+    Right goal     -> nextSol (solveWith programm goal strategy)
+    -- Error Handling
+    Left  errorStr -> putError ("ParseError: " ++ errorStr) session
+  where 
+    _goal = parse goalStr :: Either String Goal
+    programm = getProg session
+    strategy = getStrat session
+
+    nextSol :: [Subst] -> IO ()
+    nextSol []         = putStrLn "No more solutions."
+    nextSol (sol:sols) = do putStr (show sol)
+                            str <- getLine  
+                            if strip str == ";" -- TODO Proper condition ?
+                              then nextSol sols
+
+
+-- -----------------------------------------------------------------------------
+-- ********************** HELPER SECTION ***************************************
+-- _____________________________________________________________________________
+
+-- / String to welcome user.
+helloString :: String
+helloString = """Welcome!
+                 Type \":h\" for help."""
+
+-- / String to be printed when user asks for help.
 helpString :: String
 helpString =
  "Commands available from the prompt:
@@ -49,55 +127,7 @@ helpString =
     :s <strat>  Sets the specified search strategy
                 where <strat> is one of 'dfs', 'bfs', or 'iddfs'"
 
-
-expectoCommando :: Session -> IO ()
-expectoCommando session = 
-  do putStr "?- "
-     cmd <- getLine
-     case cmd of 
-       (':':(sc:arg) -> do newSession <- (getDirective session sc) (strip arg)
-                           expectoCommando newSession
-        goal         -> do s <- directiveSolve session goal
-
-
-executeDirective :: IO ()
-executeDirective session = 
-  do newSession <- (getDirective session sc) (strip arg)
-     expectoCommando newSession
-
-registerDirective :: String -> IO()
-registerDirective  "<>"
-
-getDirective :: Char -> String -> Session -> IO Session
-getDirective 
-
-
-directiveHelp :: String -> Session -> IO Session
-directiveLoad :: String -> Session ->IO Session
-directiveQuit :: String -> Session ->IO Session
-directiveReload :: String -> Session ->IO Session
-directiveStrategy :: String -> Session ->IO Session
-
-
-parse "a(0,B,_),b." :: Either String Goal
-
-parseFile "./PrologTestExamples/anon.pl" :: IO (Either String Prog)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- / Helper Funktion
+-- / Helper Funktion, to trim white spaces on the begin and end of a string.
 strip :: String -> String
 strip str =  reverse (stripFront (reverse (stripFront str)))
  where
@@ -106,71 +136,10 @@ strip str =  reverse (stripFront (reverse (stripFront str)))
   stripFront ('\t':r) = stripFront r
   stripFront str      = str
 
--- / A Session for the commandline interpreter
-data Session = Session 
-                 Maybe String  -- ^Path to current Program
-                 Prog          -- ^Current loaded Programm
-                 Strategy      -- ^Evaluating strategy
-                 Bool          -- ^is alive
-
-startSession :: Session
-startSession = Session Nothing (Prog []) dfs
-
-main :: IO ()
-main = do printHallo
-          repl startSession
-
-
-repl :: Maybe Session -> IO ()
-repl Nothing = putStrLn "Bye bye."
-repl Just  s = do putStr "?- "
-                inp <- getLine -- read
-                case strip inp of 
-                  -- inp is a command
-                  (':':(c:arg)) -> do newSession <- getCommand c (strip arg) session  --evaluate and print
-                                      repl newSession -- loop
-                  -- inp is a query
-                  goalStr       -> do evalQuery goalStr session
-                                      repl s
-                  
-getCommand :: Char -> String -> Session -> IO Session
-getCommand 'q' _ _ = 
-
-
-
-evalQuery :: String -> Session -> IO ()
-evalQuery goalStr session@(Session _ prog strat) = nextSol solutions
-  where 
-    _goal = parse goalStr :: Either String Goal -- TODO Fehlerbehandlung
-    case _goal of
-      Left  errorStr   -> …
-      Right (Goal lst) -> …
-    solutions = solveWith prog goal strat
-
-    nextSol :: [Subst] -> IO ()
-    nextSol []         = putStrLn "No more solutions."
-    nextSol (sol:sols) = do putStr (show sol)
-                            str <- getLine  
-                            if strip str == ";" -- TODO Proper condition ?
-                              then nextSol sols
-                              else return ()
-
-
-              
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- / Helper Funktion to print errors and keep old state (session).
+putError ::  String -> Session -> IO Session
+putError errorStr oldSession = do putStrLn errorStr
+                                  return oldSession
 
 
 
